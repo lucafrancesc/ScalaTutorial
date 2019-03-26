25636d768d36938e3cddfb74f22a1f5c60456a4b
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 18:35:48 +0000

    Learning how to use funcitons

---
+++ Functions.scala
@@ -0,0 +1,16 @@
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+object Functions{
+  def main(args: Array[String]){
+
+    // def getSum(int1 : Int, int2 : Int) : Int = {
+    //   int1 + int2
+    // }
+    //
+    // println(getSum(4,5))
+  }
+}

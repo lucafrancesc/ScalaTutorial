7e6f59125a7ba2b860a77c4227f316b75a348738
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 16:58:07 +0000

    Hello World

---
+++ HelloWorld.scala
@@ -0,0 +1,10 @@
+object HelloWorld {
+   /* This is my first java program.
+   * This will print 'Hello World' as the output
+   */
+   def main(args: Array[String]) {
+     val s = "hello";
+     println(s)
+      // println("Hello, world!") // prints Hello World
+   }
+}

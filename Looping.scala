bafccfaaef7dd90e09b5df9a639a9ee569e29ac6
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 17:32:35 +0000

    Creating a list and an array

--- Looping.scala
+++ Looping.scala
@@ -5,30 +5,41 @@ object Looping {
     var i = 0;
 
     // do ... while loop
-    do {
-        println(i)
-        i += 1
-    } while(i <= 10)
+    // do {
+    //     println(i)
+    //     i += 1
+    // } while(i <= 10)
 
     // while loop
-    while(i <= 10){
-      println(i*5)
-      i += 1
-    }
+    // while(i <= 10){
+    //   println(i*5)
+    //   i += 1
+    // }
 
     // for loop
-    val aList = List(1,2,3,4,5)
-    for(i <- aList){
-      println("List item " + i)
-    }
+    // val aList = List(1,2,3,4,5)
+    // for(i <- aList){
+    //   println("List item " + i)
+    // }
 
     // creating an evenlist
-    var evenList = for { i <- 1 to 20
-                      if (i % 2) == 0
-                    } yield i
-    for(i <- evenList){
-      println(i)
+    // var evenList = for { i <- 1 to 20
+    //                   if (i % 2) == 0
+    //                 } yield i
+    // for(i <- evenList){
+    //   println(i)
+    // }
+
+    for( i <- 1 to 5; j <- 6 to 10){
+      println("i value is: " + i)
+      println("j value is: " + j)
     }
+
+    val x = (1 to 10).toList
+    println(x)
+
+    val y = (1 to 10).toArray
+    println(y)
 
    }
 }

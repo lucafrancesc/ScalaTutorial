c86145b459602ab85ac2b24c05dd3789600070f3
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 18:13:50 +0000

    String interpolation

--- InputOutput.scala
+++ InputOutput.scala
@@ -1,16 +1,27 @@
 import scala.io.StdIn.{readLine, readInt}
 import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
 
 object InputOutput{
   def main(args: Array[String]){
 
-    var numberGuess = 0
-    
-    do {
-      println("Guess a number: ")
-      numberGuess = readLine.toInt
-    } while(numberGuess != 15)
+    // var numberGuess = 0
+    //
+    // do {
+    //   println("Guess a number: ")
+    //   numberGuess = readLine.toInt
+    // } while(numberGuess != 15)
+    //
+    // printf("You guessed the secret number %d\n", 15)
 
-    printf("You guessed the secret number %d\n", 15)
+    var name = "Luca"
+    var age = 29
+    var weight = 76.5
+
+    println(s"Hello, my name is ${name}")
+    println(f"I am ${age} and weight ${weight}%.2f")
+
   }
 }

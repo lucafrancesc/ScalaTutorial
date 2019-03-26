186fa5ab36fa664b26cc19246caba5029cea63ed
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 17:17:02 +0000

    for loop

--- Looping.scala
+++ Looping.scala
@@ -1,18 +1,24 @@
 object Looping {
 
   def main(args: Array[String]) {
-
+    // var declaration
     var i = 0;
+
+    // do ... while loop
     do {
         println(i)
         i += 1
     } while(i <= 10)
 
+    // while loop
+    while(i <= 10){
+      println(i*5)
+      i += 1
+    }
 
-    var j = 0
-    while(j <= 10){
-      println(j*5)
-      j += 1
+    // for loop
+    for(i <-1 to 10){
+      println(i)
     }
 
    }

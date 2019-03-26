d93437e448206adbb55907203f08b40605a684ee
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 17:41:20 +0000

    Removed obsolete files

--- Demo.scala
+++
@@ -1,9 +0,0 @@
-object Demo {
-  def main(args: Array[String]){
-    var myVar :Int = 10;
-    val myVal :String = "Hello Scala with datatype declaration.";
-    var myVar1 = 20;
-    val myVal1 = "Hello Scala new without datatype declaration.";
-    println(myVar + "\n" + myVar1 + "\n" + myVal + "\n" + myVal1);
-  }
-}

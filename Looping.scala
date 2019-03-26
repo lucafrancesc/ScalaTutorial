5c3d2db8b394c8d294eb738a8a8b650e8d3bfc5e
Luca <eto.lucafrancesco@gmail.com>
2019-03-26 17:08:22 +0000

    do...while loop

---
+++ Looping.scala
@@ -0,0 +1,19 @@
+object Looping {
+
+  def main(args: Array[String]) {
+
+    var i = 0;
+    do {
+        println(i)
+        i += 1
+    } while(i <= 10)
+
+
+    var j = 0
+    while(j <= 10){
+      println(j*5)
+      j += 1
+    }
+
+   }
+}
--- WhileLoop.scala
+++
@@ -1,9 +0,0 @@
-object WhileLoop {
-  def main(args: Array[String]) {
-    var i = 0;
-    while(i <= 10){
-      println(i)
-      i += 1
-     }
-   }
-}

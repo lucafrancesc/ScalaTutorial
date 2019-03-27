67cd770674b9828e3f98f431876b0551ab1638f6
Luca <eto.lucafrancesco@gmail.com>
2019-03-27 10:42:55 +0000

    created a mutable map

--- Maps.scala
+++ Maps.scala
@@ -6,13 +6,26 @@ import scala.io.Source
 
 object Maps{
   def main(args: Array[String]){
-    val employees = Map(
-                        "Manager" -> "Bob Smith",
-                        "Secretary" -> "Sue Brown"
-                        )
+
+//    immutable
+    val employees = Map("Manager" -> "Bob Smith", "Secretary" -> "Sue Brown")
 
     if (employees.contains("Manager"))
       printf("Manager: %s\n", employees("Manager"))
+
+//    mutable
+
+    val customers = collection.mutable.Map(100 -> "Paul Paul", 101 -> "Mark Mark")
+    println(customers)
+    customers(100) = "Jack Jack"
+    customers(103) = "Shark Shark"
+    println ( customers )
+    customers(102) = "Big Tuna"
+    println ( customers )
+    for((customerId, customerName) <- customers){
+      printf("%d -> %s\n", customerId, customerName)
+    }
+
 
   }
 }

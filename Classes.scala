3b623230a03e028d7448b1f9aa5698063dd34038
Luca <eto.lucafrancesco@gmail.com>
2019-03-27 12:19:03 +0000

    Class updated and created a second obj

--- Classes.scala
+++ Classes.scala
@@ -6,12 +6,18 @@ object Classes{
     rover.setName("Rover")
     rover.setSound("Woof")
     printf("%s says %s\n", rover.getName, rover.getSound)
+    rover.setMovement("wiggles his tail")
+    printf("%s %s\n", rover.getName, rover.getMovement)
+
+    val whiskers = new Animal("Whiskers", "Meow", "Run")
+    println(s"${whiskers.getName} with id ${whiskers.id} says ${whiskers.getSound}")
+    println(whiskers.toString)
 
   } // END OF MAIN
 
 //  CREATING MY FIRST CLASS
 // defining the default constructor
-  class Animal(var name: String, var sound : String){
+  class Animal(var name: String, var sound : String, var movement : String){
 
     this.setName(name)
 
@@ -19,6 +25,7 @@ object Classes{
 
     def getName() : String = name
     def getSound() : String = sound
+    def getMovement() : String = movement
 
     def setName(name : String){
       if(!(name.matches(".*\\d+.*")))
@@ -31,13 +38,17 @@ object Classes{
       this.sound = sound
     }
 
+    def setMovement(movement : String){
+      this.movement = movement
+    }
+
     def this(name : String){
-      this("No name", "No sound")
+      this("No name", "No sound", "No movement")
       this.setName(name)
     }
 
     def this(){
-      this("No name", "No sound")
+      this("No name", "No sound", "No movement")
     }
 
     override def toString() : String = {

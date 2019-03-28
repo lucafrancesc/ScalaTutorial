f9bcb3104f2223890d18408db44ee7deb4960449
Luca <eto.lucafrancesco@gmail.com>
2019-03-28 20:37:03 +0000

    calculate the logarithm base 10

--- HigherOrderFunctions.scala
+++ HigherOrderFunctions.scala
@@ -1,6 +1,15 @@
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
 object HigherOrderFunctions{
 
   def main(args: Array[String]): Unit = {
+
+    val log10Func = log10 _
+    println(log10Func(1000))
 
   } //END OF MAIN
 

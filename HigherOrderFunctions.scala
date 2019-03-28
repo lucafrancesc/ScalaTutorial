2fad690c0ef8bf2af01808a2f00996b9e39b738e
Luca <eto.lucafrancesco@gmail.com>
2019-03-28 20:57:24 +0000

    passing a function as argument

--- HigherOrderFunctions.scala
+++ HigherOrderFunctions.scala
@@ -10,18 +10,28 @@ object HigherOrderFunctions{
 
     val log10Func = log10 _
 
+    def time3(num : Int) = num * 3
+    def time4(num : Int) = num * 4
+
+    def multIt(func : (Int) => Double, num : Int) = {
+      func(num)
+    }
+
+    printf("3 * 100 = %.1f\n", multIt(time3, 100))
+    printf("4 * 100 = %.1f\n", multIt(time4, 100))
+
     // println(log10Func(1000))
-
-    List(100.00, 1000.00, 10000.00).map(log10Func).foreach(println)
-    println
-
-    List(1, 2, 3, 4, 5).map(_ * 50).foreach(println)
-    println
-
-    List(1, 2, 3, 4, 5).filter((x : Int ) => x % 2 == 0).foreach(println)
-    println
-
-    List(1, 2, 3, 4, 5).filter(_ % 2 != 0).foreach(println)
+    //
+    // List(100.00, 1000.00, 10000.00).map(log10Func).foreach(println)
+    // println
+    //
+    // List(1, 2, 3, 4, 5).map(_ * 50).foreach(println)
+    // println
+    //
+    // List(1, 2, 3, 4, 5).filter((x : Int ) => x % 2 == 0).foreach(println)
+    // println
+    //
+    // List(1, 2, 3, 4, 5).filter(_ % 2 != 0).foreach(println)
 
   } //END OF MAIN
 

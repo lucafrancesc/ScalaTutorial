21d83f095e34d17f9567ea651358da2472cafccb
Luca <eto.lucafrancesco@gmail.com>
2019-03-28 20:38:17 +0000

    mapping through a list calculating the log of its elements

--- HigherOrderFunctions.scala
+++ HigherOrderFunctions.scala
@@ -9,8 +9,10 @@ object HigherOrderFunctions{
   def main(args: Array[String]): Unit = {
 
     val log10Func = log10 _
-    println(log10Func(1000))
 
+    // println(log10Func(1000))
+
+    List(100.00, 1000.00, 10000.00).map(log10Func).foreach(println)
   } //END OF MAIN
 
 } // END OF OBJ CLASSES
